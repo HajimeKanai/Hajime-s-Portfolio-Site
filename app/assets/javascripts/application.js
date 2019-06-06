808832840369c5e61e6d43dea10afcba17d05d8a
Hajime Kanai <kanai027peace@gmail.com>
2019-06-06 14:56:40 +0900

    initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,41 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// = require jquery
+//= require jquery_ujs
+
+$(function(){
+    $('.top-img').css('display', 'none').fadeIn(2000);
+    $('.text').hide().slideDown(3000);
+
+    var pagetop = $('.pagetop');
+
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+        }
+    });
+
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 1000);
+        return false; // ←超重要：これがないとアニメーションが一度しか効かない
+    });
+
+    $('.perth02').one('mouseover', function(){
+        $('#icon').css('display', 'none').slideDown(1000);
+    });
+
+    $('.bxslider').bxSlider({
+        auto: true,
+        pager: true,
+    });
+    
+    lightbox.option({
+        'resizeDuration': 500,
+        'wrapAround': true
+      })
+});
+
